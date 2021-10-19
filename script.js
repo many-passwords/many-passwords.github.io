ac2f1dcdfb00a9d978a6b2e974d019eb701dfe29
Surendar <surendarsingh2002@gmail.com>
2021-10-20 02:05:09 +0530

    Sticky Navbar Updated

--- script.js
+++ script.js
@@ -106,3 +106,13 @@ $(window).scroll(function (event) {
     scrollButton.fadeIn();
   }
 });
+
+// Navbar Responsive JavaScipt
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
