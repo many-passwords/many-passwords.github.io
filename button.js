3be575f594eb8df45d86edeb1bd97462bd0f488c
Michał Piechowiak <36797326+piechowiakmichal@users.noreply.github.com>
2021-11-12 23:36:20 +0100

    Update button.js

--- button.js
+++ button.js
@@ -1,23 +1,23 @@
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
+(function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
         'gtm.start': new Date().getTime(),
         event: 'gtm.js'
-      });
-      var f = d.getElementsByTagName(s)[0],
+    });
+    var f = d.getElementsByTagName(s)[0],
         j = d.createElement(s),
         dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src =
+    j.async = true;
+    j.src =
         'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-NWX65X3');
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-NWX65X3');
 
 const downloadCSV = async () => {
     /* Download CSV file */
     try {
         const response = await fetch("https://raw.githubusercontent.com/many-passwords/many-passwords/main/passwords.csv"); // fetch data from file
-        const blob = await response.blob(); 
+        const blob = await response.blob();
 
         // create a link element to download file
         const link = document.createElement("a");
@@ -25,7 +25,7 @@ const downloadCSV = async () => {
         link.download = "passwords.csv";
         link.click();
         link.remove();
-    } catch(err) {
+    } catch (err) {
         window.alert("There is an error occur, plesea try again later!");
     }
 };
@@ -35,16 +35,16 @@ const downloadJSON = async () => {
     /* Download JSON file */
     let result = await fetch(
         "https://raw.githubusercontent.com/many-passwords/many-passwords/main/passwords.csv"
-      )
+    )
         .then((res) => res.text())
         .catch(() => "error");
-    
-    
+
+
     if (result !== "error") {
         const jsonString = csvToJSON(result);
 
         // Using blob url to resolve the error
-        let uriContent = URL.createObjectURL(new Blob([jsonString], {type: 'text/json;charset=utf-8'}))
+        let uriContent = URL.createObjectURL(new Blob([jsonString], { type: 'text/json;charset=utf-8' }))
 
         // Create a link element to download json file
         const link = document.createElement("a");
@@ -84,7 +84,7 @@ const csvToJSON = (csvText) => {
         }
         result.push(obj);
     }
-    
+
 
     return JSON.stringify(result);
 }
